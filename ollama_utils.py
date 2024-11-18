ba4b519a554147bacfbe452e4bba5c51af77ab45
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-11-18 17:03:29 +0530

    Update ollama_utils.py

--- ollama_utils.py
+++ ollama_utils.py
@@ -40,7 +40,7 @@ def install_ollama():
         return False
 
 
-def start_and_check_ollama():
+def start_and_check_ollama(model_name):
     print("Starting Ollama server...")
     subprocess.Popen(["ollama", "serve"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
@@ -63,7 +63,7 @@ def start_and_check_ollama():
     try:
         response = requests.get("http://localhost:11434/api/tags")
         models = json.loads(response.text)["models"]
-        model_info = next((m for m in models if m["name"] == "hf.co/bartowski/Meta-Llama-3.1-70B-Instruct-GGUF:Q3_K_L"), None)
+        model_info = next((m for m in models if m["name"] == model_name), None)
         if model_info:
             print("Ollama model is already downloaded!")
             wait_time = 200
@@ -76,7 +76,7 @@ def start_and_check_ollama():
 
     # Start the Ollama model
     print("Starting Ollama model...")
-    subprocess.Popen("ollama run hf.co/bartowski/Meta-Llama-3.1-70B-Instruct-GGUF:Q3_K_L > /dev/null 2>&1 &",shell=True)
+    subprocess.Popen(f"ollama run {model_name} > /dev/null 2>&1 &",shell=True)
 
     # Check if the Ollama model is running
     print("Checking if Ollama model is running...")
@@ -85,7 +85,7 @@ def start_and_check_ollama():
             response = requests.post(
                 "http://localhost:11434/api/generate",
                 json={
-                    "model": "hf.co/bartowski/Meta-Llama-3.1-70B-Instruct-GGUF:Q3_K_L",
+                    "model": model_name,
                     "prompt": "What is the importance of AI?",
                     "options": {"num_predict": 1},
                     "stream": False
